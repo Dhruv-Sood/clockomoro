8b98de042b84a8b95ad97ce715c96aa9fffb0ad1
Dhruv Sood <dhruvsoodboss@gmail.com>
2023-08-02 00:06:26 +0530

    Initialized TimerPage.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import HomePage from "./pages/HomePage";
+import TimerPage from "./pages/TimerPage";
 
 function App() {
   return (
     <>
-      <HomePage />
+      {/* <HomePage /> */}
+      <TimerPage />
     </>
   );
 }
