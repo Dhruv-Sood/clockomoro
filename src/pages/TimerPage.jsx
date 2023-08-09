6842f5800c18788c362a37707342c1d7b8156c5a
Dhruv Sood <dhruvsoodboss@gmail.com>
2023-08-09 21:41:33 +0530

    Make the timerpage responsive

--- src/pages/TimerPage.jsx
+++ src/pages/TimerPage.jsx
@@ -3,6 +3,7 @@ import { useContext , useEffect } from "react"
 
 import { DarkModeContext } from "../contexts/DarkModeContext"
 import './TimerPage.css'
+import Timer from "../components/Timer"
 function TimerPage() {
 
     const { darkMode } = useContext(DarkModeContext)
@@ -12,12 +13,12 @@ function TimerPage() {
     return (
         <>
             <Navbar />
-            <div className={`h-remaining grid place-content-center ${darkMode ? "bg-black text-white" : "bg-white text-black"}`}>
-                <div className="w-[800px] h-[400px] rounded-xl border border-red-500 shadow-custom">
+            <div className={`h-remaining grid place-content-center p-4 ${darkMode ? "bg-black text-white" : "bg-white text-black"}` }>
+                <div className="lg:px-[150px] max-w-[1000px] h-[400px] rounded-xl border border-red-500 shadow-custom">
                     <div className="flex flex-col justify-center items-center">
 
                         <div className=" h-[80px] pt-2 px-2">
-                            <h1 className=" tektur font-bold tracking-wider text-4xl m-1 gradient-red ">Code with timer, take breaks</h1>
+                            <h1 className=" tektur font-bold tracking-wider text-3xl lg:text-4xl m-1 gradient-red ">Code with timer, take breaks</h1>
                             <div className="h-1 gradient-underline w-full rounded-3xl" />
                         </div>
 
@@ -26,6 +27,7 @@ function TimerPage() {
                                 START TIMER
                             </div>
                         </div>
+                        {/* <Timer /> */}
 
                         </div>
                 </div>
