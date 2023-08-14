813fef54db8206af2f63962840e88d4766b0825f
Dhruv Sood <dhruvsoodboss@gmail.com>
2023-08-14 22:21:08 +0530

    Initialize timer

--- src/pages/TimerPage.jsx
+++ src/pages/TimerPage.jsx
@@ -16,7 +16,7 @@ function TimerPage() {
         }`}
       >
         <div className="lg:px-[150px] max-w-[1000px] h-[400px] rounded-xl border border-red-500 shadow-custom">
-          <div className="flex flex-col justify-center items-center">
+          <div className="flex flex-col gap-[20%] items-center h-full">
             <div className=" h-[80px] pt-2 px-2">
               <h1 className=" tektur font-bold tracking-wider text-3xl lg:text-4xl m-1 gradient-red ">
                 Code with timer, take breaks
@@ -24,12 +24,18 @@ function TimerPage() {
               <div className="h-1 gradient-underline w-full rounded-3xl" />
             </div>
 
-            <div className="h-[312px] flex justify-center items-center w-full">
-                          <div className={`cutive px-4 py-2 rounded-2xl  hover:scale-[1.04] hover:cursor-pointer transition-all duration-[0.35s] ${darkMode ? "bg-white text-black" : "bg-red-400 text-white"}`}>
+            {/* <div className="h-[312px] flex justify-center items-center w-full">
+              <div
+                className={`cutive px-4 py-2 rounded-2xl  hover:scale-[1.04] hover:cursor-pointer transition-all duration-[0.35s] ${
+                  darkMode ? "bg-white text-black" : "bg-red-400 text-white"
+                }`}
+              >
                 START TIMER
               </div>
-            </div>
-            {/* <Timer /> */}
+            </div> */}
+
+            <Timer />
+            <button className="btn btn-success"> Stop timer</button>
           </div>
         </div>
       </div>
