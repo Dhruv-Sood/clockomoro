29e0b1b7a1738a6c0a0859e447ecd653c69e4729
Ranjeet Kumar Jena <ranjeetjena06@gmai.com>
2023-08-22 12:04:40 +0530

    FIX #20 :Start timer to Timer

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -38,7 +38,7 @@ function HomePage() {
                 darkMode ? "bg-white text-black" : "bg-red-400 text-white"
               }`}
             >
-              Start Timer
+              Timer
             </div>
           </Link>
         </div>
